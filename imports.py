a7289e2719ac0d0b19911a115d92963cd28e3f8b
airmig <airmig@gmail.com>
2021-08-27 14:29:40 -0600

    added samples using import and dir()

--- imports.py
+++ imports.py
@@ -3,10 +3,16 @@ imports.py
 includes code to reference the use of the import clause
 """
 import sys
+import functools
 
 
 def init():
     return True
 
 
+test_import = __import__('sys')
+print(test_import.modules)
 print(sys.modules)
+print(dir())
+print(dir(sys))
+print(dir(functools))
